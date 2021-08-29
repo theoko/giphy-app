7ff05653e936552cbbbfc8580457025b533d568f
edmondfrancoismillionnaires <edmondfrancoismillionnaires@gmail.com>
2021-08-29 00:23:23 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,273 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/M/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("componentWillUnmount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentWillUnmount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("updateDimensions", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.updateDimensions()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("setDarkMode", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.setDarkMode(true)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.setDarkMode(false)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.setDarkMode(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("getTrending", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.getTrending()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("handleTermChange", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.handleTermChange(" foo", "ALL")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.handleTermChange("foo ", "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.handleTermChange(false, "ALL")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            inst.handleTermChange("DELETE FROM Projects WHERE pid = %s", "ALL")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            inst.handleTermChange("foo  ", "ALL")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            inst.handleTermChange(-Infinity, undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("updateFilterState", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.updateFilterState("bc23a9d531064583ace8f67dad60f6bb")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.updateFilterState("c466a48309794261b64a4f02cfcc3d64")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.updateFilterState(9876)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            inst.updateFilterState(12345)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            inst.updateFilterState(Infinity)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("handleImageSizeSliderChange", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange({ value: "Dillenberg", target: false }, true)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange({ value: "elio@example.com", target: false }, true)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange({ value: "elio@example.com", target: true }, true)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange({ value: "Dillenberg", target: true }, false)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange({ value: "Elio", target: false }, true)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            inst.handleImageSizeSliderChange(undefined, undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
